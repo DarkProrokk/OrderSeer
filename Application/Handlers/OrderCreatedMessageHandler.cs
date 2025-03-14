0d52ab60385876be8ba24c05d321ed4284eb94c6
DarkProrok <seroshtanmakcs@gmail.com>
2025-03-14 19:11:00 +1000

    misc

--- Application/Handlers/OrderCreatedMessageHandler.cs
+++ Application/Handlers/OrderCreatedMessageHandler.cs
@@ -13,34 +13,23 @@ public class OrderCreatedMessageHandler(IUnitOfWork unitOfWork, IKafkaProducerSe
     public string Topic { get; } = "order_created";
     public async Task HandleMessage(string message, CancellationToken cancellationToken)
     {
-        var orderData = JsonSerializer.Deserialize<OrderCreatedEvent>(message);
-        if (!orderData.Validate())
+        try
         {
-            logger.LogError($"Invalid message received: {message}");
-            var cls = new CancellationTokenSource();
-            await kafkaProducerService.ProduceInDlqAsync("error", "message was empty", cancellationToken);
-            return;
+            var orderData = JsonSerializer.Deserialize<OrderCreatedEvent>(message);
+            if (!orderData.Validate())
+            {
+                logger.LogError($"Invalid message received: {message}");
+                await kafkaProducerService.ProduceInDlqAsync("error", "message was empty", cancellationToken);
+                return;
+            }
+            await unitOfWork.OrderRepository.AddAsync(orderData.UserReference, orderData.OrderReference);
+            await unitOfWork.SaveChangesAsync();
+            logger.LogInformation($"OrderCreatedMessageHandler handled message");
         }
-        
-        await unitOfWork.OrderRepository.AddAsync(orderData.UserReference, orderData.OrderReference);
-        await unitOfWork.SaveChangesAsync();
-        logger.LogInformation($"OrderCreatedMessageHandler handled message");
-
-
-        #region test
-        //
-        // Random rand  = new Random();
-        // for (int i = 0; i < 1000; i++)
-        // {
-        //     var userGuid = Guid.NewGuid();
-        //     int randomNumber = rand.Next(2, 15);
-        //     for (int j = 0; j < randomNumber; j++)
-        //     {
-        //         var orderGuid = Guid.NewGuid();
-        //         await unitOfWork.OrderRepository.AddAsync(userGuid, orderGuid);
-        //     }
-        // }
-        //
-        #endregion
+        catch (JsonException e)
+        {
+            logger.LogError($"Invalid message format received: {message}");
+            await kafkaProducerService.ProduceInDlqAsync("error", "message was empty", cancellationToken);
+        }
     }
 }
