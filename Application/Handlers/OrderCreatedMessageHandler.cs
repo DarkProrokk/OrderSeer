aede20d9f81b9e2ba489c447e0d1f4d83b7ecbf0
DarkProrokk <Seroshtanmakcs@gmail.com>
2025-03-14 17:21:51 +1000

    Add exception handle for JsonDeserialize

--- Application/Handlers/OrderCreatedMessageHandler.cs
+++ Application/Handlers/OrderCreatedMessageHandler.cs
@@ -12,18 +12,26 @@ public class OrderCreatedMessageHandler(IUnitOfWork unitOfWork, IKafkaProducerSe
     public string Topic { get; } = "order_created";
     public async Task HandleMessage(string message, CancellationToken cancellationToken)
     {
-        var orderData = JsonSerializer.Deserialize<KafkaOrderCreatedModel>(message);
-        if (!orderData.Validate())
+        try
         {
-            logger.LogError($"Invalid message received: {message}");
-            var cls = new CancellationTokenSource();
+            var orderData = JsonSerializer.Deserialize<KafkaOrderCreatedModel>(message);
+            if (!orderData.Validate())
+            {
+                logger.LogError($"Invalid message received: {message}");
+                await kafkaProducerService.ProduceInDlqAsync("error", "message was empty", cancellationToken);
+                return;
+            }
+            await unitOfWork.OrderRepository.AddAsync(orderData.UserReference, orderData.OrderReference);
+            await unitOfWork.SaveChangesAsync();
+            logger.LogInformation($"OrderCreatedMessageHandler handled message");
+        }
+        catch (JsonException e)
+        {
+            logger.LogError($"Invalid message format received: {message}");
             await kafkaProducerService.ProduceInDlqAsync("error", "message was empty", cancellationToken);
-            return;
         }
         
-        await unitOfWork.OrderRepository.AddAsync(orderData.UserReference, orderData.OrderReference);
-        await unitOfWork.SaveChangesAsync();
-        logger.LogInformation($"OrderCreatedMessageHandler handled message");
+        
 
 
         #region test
--- Application/Models/KafkaOrderCreatedModel.cs
+++ Application/Models/KafkaOrderCreatedModel.cs
@@ -6,7 +6,6 @@ public class KafkaOrderCreatedModel
 {
     public Guid UserReference { get; set; }
     public Guid OrderReference { get; set; }
-
     public bool Validate()
     {
         return UserReference != default && OrderReference != default;
