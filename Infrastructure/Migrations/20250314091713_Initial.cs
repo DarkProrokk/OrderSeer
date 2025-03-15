777698c632671520d307da957730bd2b580b9470
DarkProrok <seroshtanmakcs@gmail.com>
2025-03-15 12:06:07 +1000

    add migrations

---
+++ Infrastructure/Migrations/20250314091713_Initial.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "status",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("status_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    guid = table.Column<Guid>(type: "uuid", nullable: false),
+                    status_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("order_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_order_status_status_id",
+                        column: x => x.status_id,
+                        principalTable: "status",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_status_history",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_id = table.Column<int>(type: "integer", nullable: true),
+                    status_id = table.Column<int>(type: "integer", nullable: true),
+                    change_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    old_status_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("order_status_history_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_order",
+                        column: x => x.order_id,
+                        principalTable: "order",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "fk_status",
+                        column: x => x.status_id,
+                        principalTable: "status",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "order_status_history_old_status_id_fkey",
+                        column: x => x.old_status_id,
+                        principalTable: "status",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_status_id",
+                table: "order",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "order_guid_key",
+                table: "order",
+                column: "guid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_status_history_old_status_id",
+                table: "order_status_history",
+                column: "old_status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_status_history_order_id",
+                table: "order_status_history",
+                column: "order_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_status_history_status_id",
+                table: "order_status_history",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "status_name_key",
+                table: "status",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_status_history");
+
+            migrationBuilder.DropTable(
+                name: "order");
+
+            migrationBuilder.DropTable(
+                name: "status");
+        }
+    }
+}
